6c81feb43a9a139a147cdfbf73154d71abe1d1ff
Dave Rocker <der62@pitt.edu>
2018-09-15 15:45:02 +0200

    first attempt at implementing remove, not tested

--- DataStructures/src/data_structures/implementation/CoarseGrainedTree.java
+++ DataStructures/src/data_structures/implementation/CoarseGrainedTree.java
@@ -20,17 +20,56 @@ public class CoarseGrainedTree<T extends Comparable<T>> implements Sorted<T> {
         if(start.data.compareTo(t) == 0 && start.left == null && start.right == null){
             start = null;
         }
+        else if(start.data.compareTo(t) == 0){
+            removeStart();
+        }
         else{
-            removeNode(start, t);
+            removeNode(start.left, start, t);
         }
     }
 
-    private void removeNode(TreeNode<T> curr, T t){
+    private void removeNode(TreeNode<T> curr, TreeNode<T> prev, T t){
         if(curr.data.compareTo(t) == 0){
             if(curr.left == null && curr.right == null){
-                
+                if(prev.left == curr){
+                    prev.left = null;
+                    removed = true;
+                }
+                else{
+                    prev.right = null;
+                    removed = true;
+                }
+            }
+            else if(curr.left == null){
+                if(prev.left == curr){
+                    prev.left = curr.right;
+                    removed = true;
+                }
+                else{
+                    prev.right = curr.right;
+                    removed = true;
+                }
+            }
+            else if(curr.right == null){
+                if(prev.left == curr){
+                    prev.left = curr.left;
+                    removed = true;
+                }
+                else{
+                    prev.right = curr.left;
+                    removed = true;
+                }
             }
         }
+        else{
+            if(curr.left != null){
+                removed = removeNode(curr.left, curr, t);
+            }
+            if(!removed && curr.right != null){
+                removed = removeNode(curr.right, curr, t);
+            }
+        }
+        return removed;
     }
 
     public ArrayList<T> toArrayList() {
