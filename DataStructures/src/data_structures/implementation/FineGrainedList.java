20691643b53161494d0d61de833d6e42b123cd1e
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-10-14 18:49:35 +0200

    Add fine grained list

--- DataStructures/src/data_structures/implementation/FineGrainedList.java
+++ DataStructures/src/data_structures/implementation/FineGrainedList.java
@@ -8,82 +8,97 @@ import java.util.concurrent.locks.ReentrantLock;
 import data_structures.Sorted;
 
 public class FineGrainedList<T extends Comparable<T>> implements Sorted<T> {
+    private static final class Node<T> {
+        public T data;
+        public Node<T> next = null;
+        private final Lock lock = new ReentrantLock();
 
- 
-    private Node<T> start;
-    private final Lock lock = new ReentrantLock();
+        public Node(T data) {
+            this.data = data;
+        }
 
-    public void add(T t){
-        Node<T> newNode = new Node<T>(t);
-        if(start == null){
-            start = newNode;
+        public void lock() {
+            lock.lock();
         }
-        else if(newNode.data.compareTo(start.data) < 0) {
-            newNode.next = start;
-            start = newNode;   
+
+        public void unlock() {
+            lock.unlock();
         }
-        else if(start.next == null){
-            start.next = newNode;
+
+        public static <T> void unlockNullable(Node<T> node, Lock lock) {
+            if (node == null) {
+                lock.unlock();
+            } else {
+                node.unlock();
+            }
         }
-        else {
-           addAndSort(newNode);
+    }
+
+    private Node<T> head = null;
+    private final Lock listLock = new ReentrantLock();
+
+    public void add(T t) {
+        listLock.lock();
+
+        // Find the nodes to insert the new node between
+        Node<T> previousNode = null;
+        Node<T> nextNode = head;
+        while (nextNode != null && t.compareTo(nextNode.data) > 0) {
+            nextNode.lock();
+            Node.unlockNullable(previousNode, listLock);
+
+            previousNode = nextNode;
+            nextNode = nextNode.next;
         }
+
+        // Insert the new node
+        Node<T> newNode = new Node<>(t);
+        newNode.next = nextNode;
+        if (previousNode == null) {
+            head = newNode;
+        } else {
+            previousNode.next = newNode;
+        }
+
+        Node.unlockNullable(previousNode, listLock);
     }
 
     public void remove(T t) {
-        if(start == null){
-            return;
-        }
-        else if(start.data.compareTo(t) == 0 && start.next == null){
-            start = null;
-        }
-       else if(start.data.compareTo(t) == 0){
-            start = start.next;     
-       }
-       else{
-           Node<T> curr = start.next;
-           Node<T> prev = start;
-           
-           while(curr != null){
-               if(curr.data.compareTo(t) == 0){
-                       prev.next = curr.next;
-                       return;
-               }
-                prev = prev.next;
-                curr = curr.next;
-               }
-           }
-       }
+        listLock.lock();
 
-    public ArrayList<T> toArrayList() {
-        if(start == null) return new ArrayList<T>();
+        // Find the node to be removed
+        Node<T> previousNode = null;
+        Node<T> currentNode = head;
+        while (currentNode != null && t.compareTo(currentNode.data) > 0) {
+            currentNode.lock();
+            Node.unlockNullable(previousNode, listLock);
 
-        ArrayList<T> list = new ArrayList<>();
-        Node<T> curr = start;
-        while(curr != null){
-            list.add(curr.data);
-            curr = curr.next;
+            previousNode = currentNode;
+            currentNode = currentNode.next;
         }
-        return list;      
+
+        // If we found it, remove it
+        if (currentNode != null && t.compareTo(currentNode.data) == 0) {
+            currentNode.lock();
+
+            // Replace the current node with the modified subtree
+            if (previousNode == null) {
+                head = currentNode.next;
+            } else {
+                previousNode.next = currentNode.next;
+            }
+        }
+
+        Node.unlockNullable(previousNode, listLock);
     }
 
-    private void addAndSort(Node<T> newNode){
-        Node<T> prev = start;
-        Node<T> curr = start.next;
-        
-        while(curr != null){
-            if(newNode.data.compareTo(curr.data)< 0){
-                newNode.next = curr;
-                prev.next = newNode;
-                return;
-            }
-            if(curr.next == null){
-                curr.next = newNode;
-                break;
-            }
-                prev = curr;
-                curr = curr.next;
-            
+    public ArrayList<T> toArrayList() {
+        ArrayList<T> list = new ArrayList<>();
+
+        for (Node<T> curr = head; curr != null; curr = curr.next) {
+            list.add(curr.data);
         }
+
+        return list;
     }
 }
